6d13d96eb25403372d50a30ac94cd8c6d6b8bc93
Pedro Augusto <pedroaugustonyo@gmail.com>
2026-09-16 10:47:34 -0300

    chore: add widget preview configuration

---
+++ mobile/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:50214/SNthB4YxOq4=';
+const String kWidgetPreviewService =
+    'widget-preview-27720128-f36a-4605-b980-6aa8f167f822';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-27720128-f36a-4605-b980-6aa8f167f822';
+const String kProjectRootPath =
+    r'/Users/pedroaoliveira/Workspace/uniube/uniube-fluently/mobile';
