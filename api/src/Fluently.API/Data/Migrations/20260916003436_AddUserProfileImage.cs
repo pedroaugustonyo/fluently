1d508357e62b65281aefa63911505a21d96aa772
Pedro Augusto <pedroaugustonyo@gmail.com>
2026-09-16 09:07:08 -0300

    feat: add profile image migration

---
+++ api/src/Fluently.API/Data/Migrations/20260916003436_AddUserProfileImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fluently.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageBase64",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImageBase64",
+                table: "Users");
+        }
+    }
+}
